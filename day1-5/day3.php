f489b1d8d36256b9c91fe74588f9283a69668cbd
Cyril Bellec <cbellec@normandiewebschool.fr>
2023-12-12 13:24:24 +0100

    update tech

--- day1-5/day3.php
+++ day1-5/day3.php
@@ -1,72 +1,59 @@
 <?php
 
-// Fonction pour calculer la priorité d'un élément
-function calculatePriority($item) {
-    $asciiValue = ord($item);
-    if ($asciiValue >= ord('a') && $asciiValue <= ord('z')) {
-        // Lowercase item
-        return $asciiValue - ord('a') + 1;
-    } elseif ($asciiValue >= ord('A') && $asciiValue <= ord('Z')) {
-        // Uppercase item
-        return $asciiValue - ord('A') + 27;
-    }
-    // Si l'élément n'est ni en minuscules ni en majuscules
-    return 0;
+$rucksacks = file ( __DIR__ . '/input-day3.txt', FILE_IGNORE_NEW_LINES );
+
+$prio_sum = 0;
+
+foreach ( $rucksacks as &$rucksack )
+{
+    $compartment_size = strlen ( $rucksack ) / 2;
+
+    $compartments = str_split ( $rucksack, $compartment_size );
+
+    // items are only counted once, even if they appear multiple times in
+    // the same compartment hence we unify...
+
+    $content = str_split ( $compartments [ 0 ] );
+
+    $content = array_unique ( $content );
+
+    foreach ( $content as $item )
+        if ( strpos ( $compartments [ 1 ], $item ) !== false )
+            $prio_sum += getPriority ( $item );
 }
 
-// Fonction pour trouver l'élément commun entre trois compartiments
-function findCommonItem($compartment1, $compartment2, $compartment3) {
-    $commonItems = array_intersect(str_split($compartment1), str_split($compartment2), str_split($compartment3));
-    return empty($commonItems) ? null : current($commonItems);
+echo "First part: $prio_sum\n";
+
+$badge_sum = 0;
+
+while ( count ( $rucksacks ) )
+{
+    $group = [
+        array_pop ( $rucksacks ),
+        array_pop ( $rucksacks ),
+        array_pop ( $rucksacks )
+    ];
+
+    // sort by rucksack size to compare the fewest items
+    usort ( $group, function ( $a, $b ){
+        return strlen ( $a ) > strlen ( $b );
+    });
+
+    for ( $i = 0; $i < strlen ( $group [ 0 ] ); $i++ )
+        if (    strpos ( $group [ 1 ], $group [ 0 ][ $i ] ) !== false
+             && strpos ( $group [ 2 ], $group [ 0 ][ $i ] ) !== false )
+        {
+            $badge_sum += getPriority ( $group [ 0 ][ $i ] );
+            break;
+        }
 }
 
-// Lire le contenu du fichier
-$inputFile = "input-day3.txt";
-$lines = file($inputFile, FILE_IGNORE_NEW_LINES);
+echo "Second part: $badge_sum\n";
 
-// Initialiser la somme des priorités pour la première partie du puzzle
-$totalPriorityPart1 = 0;
+function getPriority ( $item )
+{
+    $val = ord ( $item );
 
-// Initialiser la somme des priorités pour la deuxième partie du puzzle
-$totalPriorityPart2 = 0;
-
-// Parcourir chaque ligne du fichier
-foreach ($lines as $line) {
-    // Diviser la ligne en deux compartiments
-    $length = strlen($line) / 2;
-    $compartment1 = substr($line, 0, $length);
-    $compartment2 = substr($line, $length);
-
-    // Trouver l'élément commun entre les deux compartiments
-    $commonItem = array_intersect(str_split($compartment1), str_split($compartment2));
-
-    // S'il y a un élément commun, calculer et ajouter la priorité pour la première partie
-    if (!empty($commonItem)) {
-        $priority = calculatePriority(current($commonItem));
-        $totalPriorityPart1 += $priority;
-    }
+    // lowercase: 97-122, uppercase: 65 - 90
+    return ( $val > 90 ) ? $val - 96 : $val - 38;
 }
-
-// Parcourir chaque groupe de trois lignes
-for ($i = 0; $i < count($lines); $i += 3) {
-    // Diviser chaque ligne en deux compartiments
-    $compartment1 = $lines[$i];
-    $compartment2 = $lines[$i + 1];
-    $compartment3 = $lines[$i + 2];
-
-    // Trouver l'élément commun entre les trois compartiments
-    $commonItem = findCommonItem($compartment1, $compartment2, $compartment3);
-
-    // S'il y a un élément commun, calculer et ajouter la priorité pour la deuxième partie
-    if ($commonItem !== null) {
-        $priority = calculatePriority($commonItem);
-        $totalPriorityPart2 += $priority;
-    }
-}
-
-// Afficher les résultats
-echo "La somme des priorités pour la première partie est : " . $totalPriorityPart1 . PHP_EOL;
-echo "La somme des priorités pour la deuxième partie est : " . $totalPriorityPart2 . PHP_EOL;
-?>
-
-
